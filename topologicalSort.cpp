56ea22dd9da2f7e613e5af5c29fb05178712b2e7
Raja kumar <raja.kumar.gce@gmail.com>
2026-05-06 01:14:33 +0530

    Add BFS method in Topological Sort

--- topologicalSort.cpp
+++ topologicalSort.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 
 using namespace std;
-
+/*=========================================== DFS =========================================== */
     void dfs(int src,vector<vector <int>> &graph,stack <int> &st,vector <int>&vis){
         vis[src] = 1;
         for(auto nbr : graph[src]){
@@ -32,6 +32,37 @@ using namespace std;
         }
         return ans;
     }
+
+/*=========================================== BFS =========================================== */
+ vector <int> topoSortBFS(int V,vector <vector <int>>& edges){ //T.C = O(n*n)
+    vector <vector <int>> graph(V);
+    vector <int> inDegree(V,0);
+    for(int i=0;i<edges.size();i++){
+        graph[edges[i][0]].push_back(edges[i][1]);
+        inDegree[edges[i][1]]++;
+    }
+    queue<int> q;
+    for(int i=0;i<V;i++){
+        if(inDegree[i]==0){
+            q.push(i);
+        }
+    }
+    vector <int> ans;
+    while(q.size()){
+        int node = q.front();
+        q.pop();
+        ans.push_back(node);
+        for(int i=0;i<graph[node].size();i++){
+            int nbr = graph[node][i];
+            inDegree[nbr]--;
+            if(inDegree[nbr]==0){
+                q.push(nbr);
+            }
+        }
+    }
+    return ans;
+ }
+
 int main() {
     int V,n; // V = no. of vertrices
     cin>>V>>n;
@@ -41,6 +72,6 @@ int main() {
         cin>>a>>b;
         edges.push_back({a,b});
     }
-    topoSort(V,edges);
+    auto a = topoSort(V,edges);
 return 0;
 }
