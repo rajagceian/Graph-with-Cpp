d6b328db51413aece7dd718f2f67890ec8c81ecd
Raja kumar <raja.kumar.gce@gmail.com>
2026-05-14 16:46:29 +0530

    Update floodFill code

--- 07_floodFill.cpp
+++ 07_floodFill.cpp
@@ -1,25 +1,28 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-    int m,n;
-    void f(vector<vector<int>>& image, int i, int j, int color,int c){
-        image[i][j] = color;
-        if(i>0 && image[i-1][j]==c) f(image,i-1,j,color,c); // up
-        if(j>0 && image[i][j-1]==c) f(image,i,j-1,color,c); // left
-        if(j<n-1 && image[i][j+1]==c) f(image,i,j+1,color,c); // right
-        if(i<m-1 && image[i+1][j]==c) f(image,i+1,j,color,c); // down
+class Solution {
+public:
+    int y[4] = {0,1,0,-1};
+    int x[4] = {-1,0,1,0};
+    bool isValid(int i,int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n) return false;
+        return true;
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        m = image.size();
-        n = image[0].size();
-        int c = image[sr][sc];
-        if (c == color) return image;   
-        f(image,sr,sc,color,c);
+    void dfs(int i,int j,int tar,vector<vector<int>>& image,vector<vector<int>>& isVis,int m,int n,int col){
+        isVis[i][j] = 1;
+        image[i][j] = col;
+        for(int k=0;k<4;k++){
+            int r = i + y[k];
+            int c = j + x[k];
+            if(isValid(r,c,m,n) && image[r][c]==tar && isVis[r][c]==0){
+                dfs(r,c,tar,image,isVis,m,n,col);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int col) {
+        int m = image.size();
+        int n = image[0].size();
+        int tar = image[sr][sc];
+        vector <vector <int>> isVis(m,vector<int>(n,0));
+        dfs(sr,sc,tar,image,isVis,m,n,col);
         return image;
     }
-
-int main() {
-
-return 0;
-}
+};
