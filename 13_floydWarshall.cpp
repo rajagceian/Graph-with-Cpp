a54860e9fd459d3c2cbbc2cd4792767fc7006a74
Raja kumar <raja.kumar.gce@gmail.com>
2026-06-05 23:17:06 +0530

    Refactor floydWarshall function for improved readability

--- 13_floydWarshall.cpp
+++ 13_floydWarshall.cpp
@@ -6,12 +6,11 @@ void floydWarshall(vector<vector<int>> &dist) {
     // Code here
 const int INF = 1e8;
 int V = dist.size();
-    for(int k = 0; k < V; k++) {
+    for(int k = 0; k < V; k++) { // to generate matrix
         for(int i = 0; i < V; i++) {
             for(int j = 0; j < V; j++) {
                 if(dist[i][k] < INF && dist[k][j] < INF) {
-                    dist[i][j] = min(dist[i][j],
-                                        dist[i][k] + dist[k][j]);
+                    dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
                 }
             }
         }
